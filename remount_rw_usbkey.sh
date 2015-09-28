0034e8500326dbfb1f9b2c87cba21829816a9367
David CHANEL <david.alexandre.chanel.ext@gmail.com>
2015-09-28 20:47:14 +0000

    Added save did file

--- remount_rw_usbkey.sh
+++ remount_rw_usbkey.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+
 sudo mount -o remount ,rw /mnt/usbdrive0
+
---
+++ save_edid.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Save edid of the current screen plugged to home folder
+
+# You should be able to write when firing this script
+
+#sudo remountrw
+
+sudo /opt/vc/tvservice -d ~/edid.dat
+
+# If you want to force edid you should do that :
+
+#sudo cp ~/edid.dat /boot
+
+# Modify /boot/config.txt by adding/modifying those lines
+
+#hdmi_force_hotplug=1
+#hdmi_edid_file=1
+
